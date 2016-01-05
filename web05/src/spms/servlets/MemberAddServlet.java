9f27410dcd1beed6939cafe4d7e677fcab7120c4
ghdcjs14 <black1990-14@hanmail.net>
2016-01-05 23:57:48 +0900

    5장 EL표기법

--- web05/src/spms/servlets/MemberAddServlet.java
+++ web05/src/spms/servlets/MemberAddServlet.java
@@ -6,6 +6,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -18,18 +19,8 @@ public class MemberAddServlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.setContentType("text/html; charset=UTF-8");
-		PrintWriter out = response.getWriter();
-		out.println("<html><head><title>회원 등록</title></head>");
-		out.println("<body><h1>회원 등록</h1>");
-		out.println("<form action='add' method='post'>");
-		out.println("이름: <input type='text' name='name'><br>");
-		out.println("이메일: <input type='text' name='email'><br>");
-		out.println("암호: <input type='password' name='password'><br>");
-		out.println("<input type='submit' value='추가'>");
-		out.println("<input type='reset' value='취소'");
-		out.println("</form>");
-		out.println("</body></html>");
+		RequestDispatcher rd = request.getRequestDispatcher("member/MemberForm.jsp");
+		rd.forward(request, response);
 	}
 
 	@Override
@@ -73,7 +64,9 @@ public class MemberAddServlet extends HttpServlet {
 //			response.addHeader("Refresh","1; url=list");
 			
 		} catch(Exception e) {
-			throw new ServletException(e);
+			request.setAttribute("error", e);
+			RequestDispatcher rd = request.getRequestDispatcher("/Error.jsp");
+			rd.forward(request, response);
 		} finally {
 			try { if(stmt != null) stmt.close(); } catch (Exception e) {}
 		}
--- web05/src/spms/servlets/MemberUpdateServlet.java
+++ web05/src/spms/servlets/MemberUpdateServlet.java
@@ -7,13 +7,17 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import spms.vo.Member;
 
 @WebServlet("/member/update")
 // 컨텍스트 매개변수로 어노테이션으로 정의 할 필요 없음
@@ -45,26 +49,22 @@ public class MemberUpdateServlet extends HttpServlet {
 			rs.next();
 			
 			response.setContentType("text/html; charset=UTF-8");
-			PrintWriter out = response.getWriter();
-			out.println("<html><head><title>회원정보</title></head>");
-			out.println("<body><h1>회원정보</h1>");
-			out.println("<form action='update' method='post'>");
-			out.println("번호 : <input type='text' name='no' value='" + request.getParameter("no") +"' readonly><br>");
-			out.println("이름 : <input type='text' name='name' value='" + rs.getString("MNAME") + "'><br>");
-			out.println("이메일 : <input type='text' name='email' value='" + rs.getString("EMAIL") + "'><br>");
-			out.println("가입일 : " + rs.getDate("CRE_DATE") + "<br>");
-			out.println("<input type='submit' value='저장'>");
-			out.println("<input type='button' value='삭제'" + " onclick='location.href=\"delete?no=" + request.getParameter("no") + "\"'>");
-			out.println("<input type='button' value='취소'" +  " onclick='location.href=\"list\"'>");
-			out.println("</form>");
-			out.println("</body></html>");
+			Member member = new Member().setNo(rs.getInt("MNO"))
+															.setName(rs.getString("MNAME"))
+															.setEmail(rs.getString("EMAIL"))
+															.setCreatedDate(rs.getDate("CRE_DATE"))	;
+			request.setAttribute("member", member);
+			
+			RequestDispatcher rd = request.getRequestDispatcher("/member/MemberUpdateForm.jsp");
+			rd.forward(request, response);
 			
 		} catch (Exception e) {
-			throw new ServletException(e);
+			request.setAttribute("error", e);
+			RequestDispatcher rd = request.getRequestDispatcher("/Error.jsp");
+			rd.forward(request, response);
 		} finally {
 			try { if(rs != null) rs.close(); } catch(Exception e) {}
 			try { if(stmt != null) stmt.close(); } catch(Exception e) {}
-			try { if(conn != null) conn.close(); } catch(Exception e) {}
 		}
 	}
 
@@ -78,11 +78,7 @@ public class MemberUpdateServlet extends HttpServlet {
 		
 		try {
 			ServletContext sc = this.getServletContext();
-			Class.forName(sc.getInitParameter("driver"));
-			conn = DriverManager.getConnection(
-							sc.getInitParameter("url"),
-							sc.getInitParameter("username"),
-							sc.getInitParameter("password"));
+			conn = (Connection)sc.getAttribute("conn");
 			stmt = conn.prepareStatement(
 							" UPDATE MEMBERS SET EMAIL=?, MNAME=?, MOD_DATE=NOW() "
 							+ " WHERE MNO = ?");
@@ -95,7 +91,9 @@ public class MemberUpdateServlet extends HttpServlet {
 			response.sendRedirect("list");
 					
 		} catch(Exception e) {
-			throw new ServletException(e);
+			request.setAttribute("error", e);
+			RequestDispatcher rd = request.getRequestDispatcher("/Error.jsp");
+			rd.forward(request, response);
 		} finally {
 			try {if(stmt != null) stmt.close(); } catch(Exception e) {}
 		}
